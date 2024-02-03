3ca72b0bfeaef49001b3f46d40180ccee64a39a6
Prameela17 <prameela.10t@gmail.com>
2024-02-03 09:09:03 +0530

    Assessment Task

---
+++ script.js
@@ -0,0 +1,17 @@
+let overalCart= document.getElementById("overalCart");
+let tabContent = document.getElementById("tabContent");
+let tabMen=document.getElementById("men");
+let tabWomen=document.getElementById("women");
+let tabKids=document.getElementById("kids");
+
+tabMen.onclick=function(){
+    console.log("mens");
+}
+
+tabWomen.onclick=function(){
+    console.log("women");
+}
+
+tabKids.onclick=function(){
+    console.log("kids");
+}
